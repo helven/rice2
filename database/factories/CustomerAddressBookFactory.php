1474f80d2e21d4a74f62befc3d0b13ea876a7175
helven <helven.squall@gmail.com>
2025-05-01 01:33:57 +0800

    - Customer Address Book factory

--- database/factories/CustomerAddressBookFactory.php
+++ database/factories/CustomerAddressBookFactory.php
@@ -12,7 +12,7 @@ class CustomerAddressBookFactory extends Factory
     public function definition(): array
     {
         return [
-            'status' => fake()->randomElement([1, 2]),
+            'status_id' => fake()->randomElement([1]),
             'is_default' => false,
             'name' => fake()->company(),
             'contact' => fake()->phoneNumber(),
@@ -21,8 +21,8 @@ class CustomerAddressBookFactory extends Factory
             'address_2' => fake()->optional(0.3)->secondaryAddress(),
             'postal_code' => fake()->postcode(),
             'city' => fake()->city(),
-            'state' => fake()->numberBetween(1, 16), // Assuming states are numbered 1-16
-            'country' => 1, // Assuming 1 is the default country code
+            'state_id' => fake()->numberBetween(1, 16), // Assuming states are numbered 1-16
+            'country_id' => 1, // Assuming 1 is the default country code
         ];
     }
 }
