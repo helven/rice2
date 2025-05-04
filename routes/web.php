78f2d087f6f49846f262bf7627280961e4c73ee8
helven <helven.squall@gmail.com>
2025-05-04 11:23:11 +0800

    empty welcome page redirect to /backend

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return redirect('/backend');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
