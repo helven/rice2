7514ab4f18edcdabec7c0495aa36555a2e3dc568
helven <helven.squall@gmail.com>
2025-06-27 13:49:03 +0800

    - use FILAMENT_PATH from env than hardcode

---
+++ config/filament.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Admin Panel Path
+    |--------------------------------------------------------------------------
+    |
+    | This value is the path where Filament will be accessible from.
+    |
+    */
+    'path' => env('FILAMENT_PATH', 'backend'),
+];
